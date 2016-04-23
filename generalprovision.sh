8e81a818dc479791e6df0193f8fbe4304a2adffb
David Kumru <davidkumru@users.noreply.github.com>
2016-04-23 17:33:03 +0200

    updates

--- generalprovision.sh
+++ generalprovision.sh
@@ -9,31 +9,12 @@ echo "--- Upgrading..."
 apt-get -y upgrade
 
 echo "--- Installing..."
-apt-get -y install git #curl cmake
-
-# echo "--- Installing Ruby..."
-# apt-get -y install libssl-dev libreadline-dev zlib1g-dev
-# 
-# git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-# git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-# 
-# rm ~/.bashrc
-# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-# echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-# # export PATH="$HOME/.rbenv/bin:$PATH"
-# # eval "$(rbenv init -)"
-# # or zshrc?
-# 
-# rbenv install 2.3.0
-# rbenv global 2.3.0
+apt-get -y install git
 
 echo "--- Autoremoving..."
 apt-get -y autoremove
 
-# echo "--- Cleaning..."
-# apt-get -y clean
-
 echo "--- Cloning dotfiles..."
-# not needed?
+# needed?
 
 echo "--- Finished..."
--- rubyprovision.sh
+++ rubyprovision.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
-# echo "--- Provisioning..."
-# 
-# echo "--- Updating..."
-# apt-get update
-# 
-# echo "--- Upgrading..."
-# apt-get -y upgrade
-# 
-# echo "--- Installing..."
-# apt-get -y install git #curl cmake
- 
-echo "--- Installing Ruby..."
-sudo apt-get -y install libssl-dev libreadline-dev zlib1g-dev
+echo "--- Installing Ruby/Rails..."
+sudo apt-get -y install libssl-dev libreadline-dev zlib1g-dev libsqlite3-dev nodejs
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
@@ -20,21 +9,11 @@ git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 rm ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-# export PATH="$HOME/.rbenv/bin:$PATH"
-# eval "$(rbenv init -)"
-# or zshrc?
 source ~/.bashrc
+# or zshrc?
 
 rbenv install 2.3.0
 rbenv global 2.3.0
+gem install rails -v 4.2.6
 
-# echo "--- Autoremoving..."
-# apt-get -y autoremove
-# 
-# # echo "--- Cleaning..."
-# # apt-get -y clean
-# 
-# echo "--- Cloning dotfiles..."
-# # not needed?
-# 
-# echo "--- Finished..."
+echo "--- Finished..."
