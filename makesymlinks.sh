d0ab911c2122a496a0e8bf27a3671a3b83fea0cf
David Kumru <davidkumru@users.noreply.github.com>
2016-04-24 11:21:54 +0200

    updates

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-############################
+
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+# This script creates symlinks from a directory to any desired dotfiles in ~/dotfiles
 
-########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .zshrc .profile .vimrc .tmux.conf"  # list of files/folders to symlink in homedir
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files=".bashrc .zshrc .profile .vimrc .tmux.conf"
+vagrant="Vagrantfile"
 
 ##########
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo -n "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "done"
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+echo -n "Changing to the $dir directory"
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the target directories to any files in the ~/dotfiles directory specified in the variables
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
+    echo "Moving any existing dotfiles to $olddir"
     mv ~/$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    echo "Creating symlink to $file"
     ln -s $dir/$file ~/$file
+done
+
+for file in $vagrant; do
+    echo "Moving any existing dotfiles to $olddir"
+    mv ~/vagrant/$file ~/dotfiles_old/
+    echo "Creating symlink to $file"
+    ln -s $dir/$file ~/vagrant/$file
 done
