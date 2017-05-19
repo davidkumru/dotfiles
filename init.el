5472bfafb606113f69fdbb40a9c3c5c0667548ab
David Kumru <davidkumru@gmail.com>
2017-05-19 14:17:30 +0200

    updates

--- init.el
+++ init.el
@@ -21,9 +21,18 @@
 (evil-mode 1)
 
 (custom-set-variables
-  '(package-selected-packages (quote (evil))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (zenburn-theme evil))))
 (custom-set-faces
-  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ; interface
 (tool-bar-mode -1)
+(load-theme 'zenburn t)
