5adc86a8e163a836d5617f1b11a2cd14523f2d2a
David Kumru <davidkumru@gmail.com>
2018-11-06 15:39:17 +0100

    updates

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -3,7 +3,7 @@
 dir=~/dotfiles
 olddir=~/.dotfiles_old
 
-files=".zshrc .vimrc .tmux.conf .gitconfig .gitignore"
+files=".zshrc .vimrc .tmux.conf .gitconfig .gitignore .tool-versions"
 
 echo "Creating $olddir"
 mkdir -p $olddir
